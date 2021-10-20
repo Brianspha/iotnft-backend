05274da2649e3d3241e9b7f6566f6d28344a5473
Siphamandla Mjoli <brianm445@yahoo.com>
2021-10-20 11:18:02 +0200

    update

---
+++ index.js
@@ -0,0 +1,49 @@
+const morgan = require("morgan");
+const app = require("restana")({});
+const cors = require("cors");
+const axios = require("axios").default;
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(cors());
+app.use(morgan("combined"));
+
+app.get("/", (req, res, next) => {
+  console.log("imei: ", req.body.imei);
+  axios({
+    url: "http://34.146.117.200:8000/subgraphs/name/iotex/pebble-subgraph",
+    method: "post",
+    data: {
+      query: `
+          {
+            deviceRecords(where: { imei:"${req.body.imei}"}) {
+              raw # !! Protobuf encoded sensors values
+              imei
+              signature
+            }
+          }`,
+    },
+  })
+    .then((result) => {
+      console.log(result.data);
+      res.statusCode = 200;
+      res.send(result.data);
+    })
+    .catch((error) => {
+      console.log("error: ", error);
+      res.statusCode = 400;
+      res.send({ data: { deviceRecords: [] } });
+    });
+});
+
+async function start() {
+  await app.start(3000, "0.0.0.0");
+  console.log(
+    "======================Server Started on PORT 3000======================"
+  );
+}
+start();
